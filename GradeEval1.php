a423c7507a3ef31efc88213e2c146a611799404c
rosinantecora23 <flora.ishmael.garin@gmail.com>
2026-01-14 02:27:08 +0800

    yes

---
+++ GradeEval1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Grade Evaluation1</title>
+	</head>
+	<body>
+		<form action = "GradeEval2.php" method = "POST">
+		Name:<input type = "text" name = "name"> <br>
+		Program:<input type = "text" name = "program"> <br>
+		Student Number:<input type = "text" name = "stnum"><br>
+		Grade:<input type = "number" name = "grade"><br>
+		<input type = "submit" value = "Submit">
+		</form>
+		
+			
+
+	</body>
+</html>
